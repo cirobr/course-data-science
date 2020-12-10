6e9cfd041b38047e6dc423d0f689e31224fbef1a
Ciro B Rosa <37887021+cirobr@users.noreply.github.com>
2020-12-10 15:52:32 -0300

    Update a25.R

--- ds8-machine-learning/a25.R
+++ ds8-machine-learning/a25.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(dslabs)
+library(rpart)
+options(digits = 3)
 data("tissue_gene_expression")
 
 
@@ -7,13 +9,12 @@ data("tissue_gene_expression")
 library(caret)
 dat <- as.data.frame(tissue_gene_expression)
 
-set.seed(1991, sample.kind="Rounding")    # will make R 3.6 generate a seed as in R 3.5
-train_rpart <- train(y ~ .,
-                     method = "rpart",
-                     data = dat,
-                     tuneGrid = data.frame(cp = seq(0, 0.1, 0.01))
-                     )
-train_rpart$bestTune
+fit_rpart <- train(y ~ .,
+                   method = "rpart",
+                   data = dat,
+                   tuneGrid = data.frame(cp = seq(0, 0.1, 0.01))
+                   )
+fit_rpart$bestTune
 
 
 # q2
@@ -24,4 +25,26 @@ train_rpart <- train(y ~ .,
                      tuneGrid = data.frame(cp = seq(0, 0.1, 0.01)),
                      control = rpart.control(minsplit = 0)
                      )
-#train_rpart$bestTune
+confusionMatrix(train_rpart)
+
+
+# q3
+fit <- rpart(y ~ ., data = dat)
+plot(fit, margin = 0.1)
+text(fit, cex = 0.75)
+
+
+#q4
+set.seed(1991, sample.kind="Rounding")    # will make R 3.6 generate a seed as in R 3.5
+fit <- train(y ~ .,
+             method = "rf",
+             data = dat,
+             tuneGrid = data.frame(mtry = seq(50, 200, 25)),
+             nodesize = 1
+)
+fit$bestTune
+ggplot(fit, highlight = TRUE)
+
+
+# q5
+imp <- varImp(fit)
