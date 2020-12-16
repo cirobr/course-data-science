e0e622aeebb162e9e5bd80750f532cd1e12e2b76
Ciro B Rosa <37887021+cirobr@users.noreply.github.com>
2020-12-16 13:43:28 -0300

    Update titanic-part-2.R

--- ds8-machine-learning/titanic-part-2.R
+++ ds8-machine-learning/titanic-part-2.R
@@ -53,4 +53,88 @@ mean(y_hat_glm == test_set$Survived)
 
 
 # q9 knn prediction
+#getModelInfo("knn")
+#modelLookup("knn")
 set.seed(6, sample.kind = "Rounding") # if using R 3.5 or earlier, remove the sample.kind argument
+train_knn <- train(Survived ~ .,
+                   method = "knn",
+                   data = train_set,
+                   tuneGrid = data.frame(k = seq(3, 51, 2)))
+ggplot(train_knn, highlight = TRUE)
+train_knn$bestTune
+
+y_hat_knn <- predict(train_knn, test_set)
+mean(y_hat_knn == test_set$Survived)
+
+confusionMatrix(y_hat_knn, test_set$Survived)$overall[["Accuracy"]]
+
+
+# q10 knn 10-fold cross-validation
+set.seed(8, sample.kind = "Rounding") # if using R 3.5 or earlier, remove the sample.kind argument
+
+control <- trainControl(method = "cv",   # stands for cross validation
+                        number = 10,     # 10-fold cross validation
+                        p = .9)          # 90% of data as train set
+
+train_knn_cv <- train(Survived ~ .,
+                      method = "knn", 
+                      data = train_set,
+                      tuneGrid = data.frame(k = seq(3, 51, 2)),
+                      trControl = control)
+ggplot(train_knn_cv, highlight = TRUE)
+train_knn_cv$bestTune
+
+y_hat_knn_cv <- predict(train_knn_cv, test_set)
+confusionMatrix(y_hat_knn_cv, test_set$Survived)$overall[["Accuracy"]]
+
+
+# q11-a decision tree method
+set.seed(10, sample.kind = "Rounding") # if using R 3.5 or earlier, remove the sample.kind argument
+
+train_rpart <- train(Survived ~ .,
+                     method = "rpart",
+                     tuneGrid = data.frame(cp = seq(0, 0.05, 0.002)),
+                     data = train_set)
+ggplot(train_rpart, highlight = TRUE)
+train_rpart$bestTune
+
+y_hat_rpart <- predict(train_rpart, test_set)
+confusionMatrix(y_hat_rpart, test_set$Survived)$overall[["Accuracy"]]
+
+# q11-b plot the decision tree
+plot(train_rpart$finalModel, margin = 0.1)
+text(train_rpart$finalModel, cex = 0.75)
+
+# q11-c predict about a number of passengers
+Sex <- c("male", "female", "female", "male", "female", "female", "male")
+Age <- c(28, 0, 0, 5, 0, 17, 17)
+Pclass <- c(0, 2, 3, 0, 3, 1, 1)
+Fare <- c(0, 0, 8, 0,25, 0, 0) 
+SibSp <- c(0, 0, 0, 4, 0, 2 ,2)
+Parch <- 0
+FamilySize <- 0
+Embarked <- ""
+
+dat <- data.frame(Sex, Age, Pclass, Fare, SibSp, Parch, FamilySize, Embarked)
+
+y_hat_rpart2 <- predict(train_rpart, dat)
+y_hat_rpart2
+
+
+# q12 random forest model
+set.seed(14, sample.kind = "Rounding") # if using R 3.5 or earlier, remove the sample.kind argument
+
+train_rf <- train(Survived ~ .,
+                  method = "rf",
+                  tuneGrid = data.frame(mtry = seq(1:7)),
+                  ntree = 100,
+                  data = train_set)
+ggplot(train_rf, highlight = TRUE)
+train_rf$bestTune
+
+y_hat_rf <- predict(train_rf, test_set)
+confusionMatrix(y_hat_rf, test_set$Survived)$overall[["Accuracy"]]
+mean(y_hat_rf == test_set$Survived)
+
+imp <- varImp(train_rf)
+imp
