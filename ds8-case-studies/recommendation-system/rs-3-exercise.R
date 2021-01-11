1da35c810f13112446329f803f24d2a9ef669ed0
Ciro B Rosa <37887021+cirobr@users.noreply.github.com>
2021-01-11 15:19:03 -0300

    Update rs-3-exercise.R

--- ds8-case-studies/recommendation-system/rs-3-exercise.R
+++ ds8-case-studies/recommendation-system/rs-3-exercise.R
@@ -7,5 +7,26 @@ data("movielens")
 # q1
 head(movielens)
 p <- movielens %>%
-  group_by(year) %>%
+  group_by(movieId, year) %>%
   summarize(nr = n())
+
+p %>%
+  ggplot(aes(year, nr, group=movieId)) +
+  geom_boxplot() +
+  scale_y_continuous(trans = "sqrt")
+
+q <- p %>%
+  ungroup() %>%
+  group_by(year) %>%
+  summarize(md = median(nr))
+head(q)
+max(q$md)
+q[which.max(q$md),]
+
+
+# q2
+p %>%
+  ungroup() %>%
+  filter(year >= 1993) %>%
+  group_by(movieId) %>%
+  summarize(mean(nr))
