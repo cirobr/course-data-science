51594d0045f542dad821b5247490d1c0a11c2f93
Ciro B Rosa <37887021+cirobr@users.noreply.github.com>
2021-01-13 19:34:07 -0300

    Update rs-3-exercise.R

--- ds8-case-studies/recommendation-system/rs-3-exercise.R
+++ ds8-case-studies/recommendation-system/rs-3-exercise.R
@@ -8,25 +8,42 @@ data("movielens")
 head(movielens)
 p <- movielens %>%
   group_by(movieId, year) %>%
-  summarize(nr = n())
+  summarize(n_ratings = n())
+head(p)
 
 p %>%
-  ggplot(aes(year, nr, group=movieId)) +
+  ggplot(aes(year, n_ratings, group=movieId)) +
   geom_boxplot() +
   scale_y_continuous(trans = "sqrt")
 
 q <- p %>%
   ungroup() %>%
   group_by(year) %>%
-  summarize(md = median(nr))
+  summarize(md = median(n_ratings))
 head(q)
 max(q$md)
 q[which.max(q$md),]
 
 
 # q2
-p %>%
-  ungroup() %>%
+movies <- movielens %>%
+  select(movieId, title) %>%
+  distinct()
+
+tab1 <- movielens %>%
   filter(year >= 1993) %>%
+  mutate(qtd_yr=2018-year) %>%
   group_by(movieId) %>%
-  summarize(mean(nr))
+  summarize(avg_rating_per_yr=mean(n()/qtd_yr)) %>%
+  arrange(desc(avg_rating_per_yr)) %>%
+  top_n(n=25)
+
+left_join(tab1, movies) %>%
+  filter(title == "Forrest Gump")
+
+tab2 <- movielens %>%
+  filter(year >= 1993)
+
+tab2 %>%
+  filter(title == "Shawshank Redemption, The") %>%
+  head()
