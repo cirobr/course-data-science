27d7ad5641e054b7a31017ec993f77901a2f3670
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2021-08-09 12:28:04 -0300

    model-logistic binary finalized

--- ds9-capstone/model-logistic.R
+++ ds9-capstone/model-logistic.R
@@ -1,7 +1,7 @@
 # R version: 4.1.0
 setwd("~/projects/data-science-course/ds9-capstone")
-subset_size = 50000
-rating_to_predict = 5
+#subset_size = 50000
+rating_to_predict = 5  # stored model only works with "5"
 
 # clean memory
 rm(edx, edx2)
@@ -14,17 +14,14 @@ library(tidyverse)
 
 options(digits = 3)
 
-#rm(train_set, test_set)
 # read dataset from csv
 if(!exists("train_set")) {train_set <- read_csv(file = "./dat/train.csv")}
-#train_set$rating <- as.factor(train_set$rating)
 if(!exists("test_set")) {test_set <- read_csv(file = "./dat/test.csv")}
-#test_set$rating <- as.factor(test_set$rating)
 
 # creates train subset for experiences with small size dataset
-train_subset <- head(train_set, n=subset_size)
+#train_subset <- head(train_set, n=subset_size)
 
-# prepare datasets for prediction of rate 5
+# prepare datasets for prediction
 m_train_set <- train_set %>%
   mutate(y = as.numeric(rating == rating_to_predict)) %>%
   select(-c(rating))
@@ -33,19 +30,18 @@ m_test_set <- test_set %>%
   mutate(y = as.numeric(rating == rating_to_predict)) %>%
   select(-c(rating))
 
-m_train_subset <- train_subset %>%
-  mutate(y = as.numeric(rating == rating_to_predict)) %>%
-  select(-c(rating))
+# m_train_subset <- train_subset %>%
+#   mutate(y = as.numeric(rating == rating_to_predict)) %>%
+#   select(-c(rating))
 
-#rm(train_set, test_set, train_subset)
-
-# fit the model
+# clean memory
 rm(train_set, test_set, train_subset)
 
-fit_log <- m_train_set %>%
-  glm(y ~ ., 
-      data=., 
-      family=binomial)
+# fit the model
+# fit_log <- m_train_set %>% glm(y ~ .,
+#                                data=.,
+#                                family=binomial)
+# load(file="./mdl/fit_log.RData")
 
 p_hat <- predict(fit_log, 
                  newdata = m_test_set, 
@@ -57,17 +53,41 @@ f_acc <- function(x){
   mean(y_hat == y)
 }
 
+f_f1 <- function(x){
+  y_hat <- ifelse(p_hat >= x, 1, 0) %>% as.numeric() %>% factor(levels = factor(c(0, 1)))
+  F_meas(data=y_hat, reference=y_fac)
+}
+
+cutoff <- seq(0.1, 0.9, 0.05)
+
 y = m_test_set$y
-cutoff <- seq(0.1, 0.9, 0.1)
 acc <- sapply(cutoff, f_acc)
 
 data.frame(cutoff, acc) %>% 
   ggplot(aes(cutoff, acc)) + 
+  ggtitle("Accuracy") +
+  scale_x_continuous(breaks = cutoff) +
   geom_point() + 
-  geom_line()
+  geom_line() +
+  geom_label(label = sprintf("%0.2f", round(acc, digits = 2)), size = 2, nudge_y = -0.05)
 
-y_hat <- ifelse(p_hat >= 0.15, 1, 0) %>% as.numeric()
-confusionMatrix(data=factor(y_hat, levels = factor(c(0, 1))), reference=factor(y, levels = factor(c(0, 1))))
+y_fac <- y %>% factor()
+f1 <- sapply(cutoff, f_f1)
+
+data.frame(cutoff, f1) %>% 
+  ggplot(aes(cutoff, f1)) + 
+  ggtitle("F1-score") +
+  scale_x_continuous(breaks = cutoff) +
+  geom_point() + 
+  geom_line() +
+  geom_label(label = sprintf("%0.2f", round(f1, digits = 2)), size = 2, nudge_y = -0.05)
+
+# cutoff = 0.5 evaluation  
+y_hat <- ifelse(p_hat >= 0.5, 1, 0) %>% as.numeric() %>% factor(levels = factor(c(0, 1)))
+sensitivity(data = y_hat, reference = y_fac)
+specificity(data = y_hat, reference = y_fac)
+F_meas(data=y_hat, reference=y_fac)
+confusionMatrix(data=y_hat, reference=y_fac)
 
 # save model
-save(fit_log, file="./mdl/fit_log.Rdata")
+# save(fit_log, file="./mdl/fit_log.RData")
--- ds9-capstone/read-csv.R
+++
@@ -1,5 +0,0 @@
-# read datasets from csv
-setwd("~/projects/data-science-course/ds9-capstone")
-
-edx <- read_csv(file = "./dat/edx.csv")
-#validation <- read_csv(file = "./dat/validation.csv")
