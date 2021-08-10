9d1a1d4b80050d61beedbc66a09234a817203e0c
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2021-08-09 22:03:24 -0300

    Update model-logistic.R

--- ds9-capstone/model-logistic.R
+++ ds9-capstone/model-logistic.R
@@ -1,7 +1,9 @@
 # R version: 4.1.0
 setwd("~/projects/data-science-course/ds9-capstone")
-#subset_size = 50000
-rating_to_predict = 5  # stored model only works with "5"
+
+# suppress warnings
+oldw <- getOption("warn")
+options(warn = -1)
 
 # clean memory
 rm(edx, edx2)
@@ -13,81 +15,86 @@ library(caret)
 library(tidyverse)
 
 options(digits = 3)
+subset_size = 500000
 
 # read dataset from csv
-if(!exists("train_set")) {train_set <- read_csv(file = "./dat/train.csv")}
-if(!exists("test_set")) {test_set <- read_csv(file = "./dat/test.csv")}
+train_set <- read_csv(file = "./dat/train.csv")
+test_set <- read_csv(file = "./dat/test.csv")
 
-# creates train subset for experiences with small size dataset
-#train_subset <- head(train_set, n=subset_size)
+# define rating to predict as the most common rating
+df <- train_set %>% select(rating) %>% group_by(rating) %>% summarize(n = n())
+df[which.max(df$n),]$rating
+rating_to_predict = 4  # stored model developed for "4"
 
-# prepare datasets for prediction
-m_train_set <- train_set %>%
+# prepare datasets for categorical analysis
+train_set <- train_set %>%
   mutate(y = as.numeric(rating == rating_to_predict)) %>%
   select(-c(rating))
 
-m_test_set <- test_set %>%
+test_set <- test_set %>%
   mutate(y = as.numeric(rating == rating_to_predict)) %>%
   select(-c(rating))
-
-# m_train_subset <- train_subset %>%
-#   mutate(y = as.numeric(rating == rating_to_predict)) %>%
-#   select(-c(rating))
-
-# clean memory
-rm(train_set, test_set, train_subset)
 
 # fit the model
-# fit_log <- m_train_set %>% glm(y ~ .,
-#                                data=.,
-#                                family=binomial)
+# fit_log <- train_set %>% 
+#   glm(y ~ userId + movieId,
+#       data=.,
+#       family=binomial)
 # load(file="./mdl/fit_log.RData")
 
 p_hat <- predict(fit_log, 
-                 newdata = m_test_set, 
+                 newdata = test_set, 
                  type = "response")
+p_hat <- as.numeric(p_hat)
+
+# rmse function definition
+RMSE <- function(true_ratings, predicted_ratings){
+  sqrt(mean((true_ratings - predicted_ratings)^2))
+}
 
 # cutoff analysis
-f_acc <- function(x){
+f_err <- function(x){
   y_hat <- ifelse(p_hat >= x, 1, 0) %>% as.numeric()
-  mean(y_hat == y)
+  RMSE(test_set$y, y_hat)
 }
 
-f_f1 <- function(x){
-  y_hat <- ifelse(p_hat >= x, 1, 0) %>% as.numeric() %>% factor(levels = factor(c(0, 1)))
-  F_meas(data=y_hat, reference=y_fac)
-}
+# f_f1 <- function(x){
+#   y_hat <- ifelse(p_hat >= x, 1, 0) %>% as.numeric() %>% factor(levels = factor(c(0, 1)))
+#   F_meas(data=y_hat, reference=y_fac)
+# }
 
 cutoff <- seq(0.1, 0.9, 0.05)
 
-y = m_test_set$y
-acc <- sapply(cutoff, f_acc)
-
-data.frame(cutoff, acc) %>% 
-  ggplot(aes(cutoff, acc)) + 
-  ggtitle("Accuracy") +
+err <- sapply(cutoff, f_err)
+data.frame(cutoff, err) %>%
+  ggplot(aes(cutoff, err)) +
+  ggtitle("RMSE") +
   scale_x_continuous(breaks = cutoff) +
-  geom_point() + 
+  geom_point() +
   geom_line() +
-  geom_label(label = sprintf("%0.2f", round(acc, digits = 2)), size = 2, nudge_y = -0.05)
+  geom_label(label = sprintf("%0.2f", round(err, digits = 2)), size = 2, nudge_y = -0.02)
 
-y_fac <- y %>% factor()
-f1 <- sapply(cutoff, f_f1)
+# y_fac <- factor(test_set$y)
+# f1 <- sapply(cutoff, f_f1)
+# data.frame(cutoff, f1) %>%
+#   ggplot(aes(cutoff, f1)) +
+#   ggtitle("F1-score") +
+#   scale_x_continuous(breaks = cutoff) +
+#   geom_point() +
+#   geom_line() +
+#   geom_label(label = sprintf("%0.2f", round(f1, digits = 2)), size = 2, nudge_y = -0.05)
 
-data.frame(cutoff, f1) %>% 
-  ggplot(aes(cutoff, f1)) + 
-  ggtitle("F1-score") +
-  scale_x_continuous(breaks = cutoff) +
-  geom_point() + 
-  geom_line() +
-  geom_label(label = sprintf("%0.2f", round(f1, digits = 2)), size = 2, nudge_y = -0.05)
-
-# cutoff = 0.5 evaluation  
-y_hat <- ifelse(p_hat >= 0.5, 1, 0) %>% as.numeric() %>% factor(levels = factor(c(0, 1)))
-sensitivity(data = y_hat, reference = y_fac)
-specificity(data = y_hat, reference = y_fac)
-F_meas(data=y_hat, reference=y_fac)
-confusionMatrix(data=y_hat, reference=y_fac)
-
+# cutoff = 0.20 evaluation
+# y_hat <- ifelse(p_hat >= 0.3, 1, 0) %>% as.numeric() %>% factor(levels = factor(c(0, 1)))
+# sensitivity(data = y_hat, reference = factor(test_set$y))
+# specificity(data = y_hat, reference = factor(test_set$y))
+# F_meas(data=y_hat, reference=factor(test_set$y))
+# cm <- confusionMatrix(data=y_hat, reference=factor(test_set$y))
+# cm$overall["Accuracy"]
+# cm
+ 
 # save model
 # save(fit_log, file="./mdl/fit_log.RData")
+
+# restore warnings
+options(warn = oldw)
